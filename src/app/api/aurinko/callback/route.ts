34c6559e575539814943cce0579a015d2c7ec2d5
Mike Cecconello <mikececco2000@gmail.com>
2024-10-17 11:14:11 +0200

    get access token of user through code from auth response

--- src/app/api/aurinko/callback/route.ts
+++ src/app/api/aurinko/callback/route.ts
@@ -1,9 +1,20 @@
 import { auth } from "@clerk/nextjs/server"
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async (req: Request) => {
+export const GET = async (req: NextRequest) => {
     const {userId} = await auth()
-    console.log(userId);
+
+    if (!userId) return NextResponse.json({message: 'Unauthroized'}, {status: 401})
+
+    const params = req.nextUrl.searchParams
+
+    const status = params.get('status')
+    if (status != 'success') return NextResponse.json({message: 'Failed to link account'}, {status: 400})
+    
+    const code = params.get('code')
+    if (code != 'success') return NextResponse.json({message: 'Failed to link account'}, {status: 400})
+    
+        console.log(userId);
 
     return NextResponse.json({ message: 'Hello' }, { status: 200 });
     
--- src/lib/aurinko.ts
+++ src/lib/aurinko.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { auth } from "@clerk/nextjs/server"
+import axios from 'axios'
 
 export const getAurinkoAuthUrl = async (serviceType: 'Google' | 'Office365') => {
     const userId = await auth() //clerk
@@ -17,4 +18,16 @@ export const getAurinkoAuthUrl = async (serviceType: 'Google' | 'Office365') => 
 
     return `https://api.aurinko.io/v1/auth/authorize?${params.toString()}`;
 }
-    
+
+export const exchangeCodeForAccessToken = async (code: string) => {
+    try {
+        const response = await axios.post('https://api.aurinko.io/v1/account', {}, {
+            auth: {
+                username: process.env.AURINKO_CLIENT_ID as string,
+                password: process.env.AURINKO_CLIENT_SECRET as string
+            }
+        })
+    } catch (error) {
+        
+    }
+}
