0e3584383b828bf591927aa81ba9b95aab027966
Mike Cecconello <mikececco2000@gmail.com>
2024-10-17 05:14:52 +0200

    user synced with database

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -1,7 +1,26 @@
 import { NextRequest } from "next/server";
+import { db } from "~/server/db";
 
 export async function POST(req: NextRequest) {
     const { data } = await req.json();
+
+    const email = data.email_addresses[0].email_address;
+
+    const firstName = data.first_name;
+    const lastName = data.last_name;
+    const imageUrl = data.image_url;
+    const id = data.id;
+
+    await db.user.create({
+        data: {
+            email,
+            firstName,
+            lastName,
+            imageUrl,
+            id,
+        },
+    });
+
 
     console.log('Webhook received', data);
 
