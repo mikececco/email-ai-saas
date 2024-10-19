3a79acbefb6e8cd1f112d2c94f32adc194ff9c7b
Mike Cecconello <mikececco2000@gmail.com>
2024-10-19 09:51:27 +0200

    fix scopes

--- src/lib/aurinko.ts
+++ src/lib/aurinko.ts
@@ -4,17 +4,17 @@ import { auth } from "@clerk/nextjs/server"
 import axios from 'axios'
 
 export const getAurinkoAuthUrl = async (serviceType: 'Google' | 'Office365') => {
-    const userId = await auth() //clerk
+    const {userId} = await auth() //clerk
 
     if (!userId) throw new Error('User not found');
 
     const params = new URLSearchParams({
         clientId: process.env.AURINKO_CLIENT_ID as string,
         serviceType,
-        scope: 'Mail.Read Mail.ReadWrite Mail.Send Mail.Drafts Mail.All',
+        scopes: 'Mail.Read Mail.ReadWrite Mail.Send Mail.Drafts Mail.All',
         response_type: 'code',
         returnUrl: `${process.env.NEXT_PUBLIC_URL}/api/aurinko/callback`,
-    });
+    });    
 
     return `https://api.aurinko.io/v1/auth/authorize?${params.toString()}`;
 }
