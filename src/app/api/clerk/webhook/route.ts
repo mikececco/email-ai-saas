c1f7e5588b6ad39897e993f6a86ab23b74c098f3
Mike Cecconello <mikececco2000@gmail.com>
2024-10-20 15:49:34 +0200

    user upsert

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -1,28 +1,43 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { db } from "~/server/db";
 
 export async function POST(req: NextRequest) {
     const { data } = await req.json();
 
-    const emailAddress = data.email_addresses[0].email_address;
+    console.log(' HERE IS DATA:');
+    console.log(data);
 
+    const emailAddress = data.email_addresses[0].email_address;
     const firstName = data.first_name;
     const lastName = data.last_name;
     const imageUrl = data.image_url;
     const id = data.id;
 
-    await db.user.create({
-        data: {
-            emailAddress,
-            firstName,
-            lastName,
-            imageUrl,
-            id,
-        },
-    });
+    console.log('GOING TO UPSERT USER');
 
+    try {
+        const user = await db.user.upsert({
+            where: { id: id },
+            update: {
+                emailAddress,
+                firstName,
+                lastName,
+                imageUrl,
+            },
+            create: {
+                id,
+                emailAddress,
+                firstName,
+                lastName,
+                imageUrl,
+            },
+        });
 
-    console.log('Webhook received', data);
+        console.log('User upserted:', user);
 
-    return new Response('Done', { status: 200 });
+        return NextResponse.json({ message: 'User processed successfully' }, { status: 200 });
+    } catch (error) {
+        console.error('Error processing user:', error);
+        return NextResponse.json({ message: 'Error processing user' }, { status: 500 });
+    }
 }
