00bc56e13dbfcb8513f7f3d08c533e34dd4c369e
Mike Cecconello <mikececco2000@gmail.com>
2024-10-29 08:53:27 +0100

    dynamic client side rendering

--- src/app/mail/page.tsx
+++ src/app/mail/page.tsx
@@ -1,4 +1,11 @@
-import Mail from './mail'
+import dynamic from 'next/dynamic'
+
+// import Mail from './mail'
+const Mail = dynamic(() => {
+  return import ('./mail')
+}, {
+  ssr: false //This approach is useful for components that rely on client-specific data or when you want to optimize server performance by reducing the rendering workload.
+})
 
 const EmailDashboard = () => {
   return (
