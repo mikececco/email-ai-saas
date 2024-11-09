134e45cc2cae2e852db925d65a5444958829226f
Mike Cecconello <mikececco2000@gmail.com>
2024-11-09 09:05:48 +0100

    Skeleton Thread emails

--- src/app/mail/thread-display.tsx
+++ src/app/mail/thread-display.tsx
@@ -1,9 +1,20 @@
 'use client'
 
-import { Archive, ArchiveX, Clock, Trash, Trash2 } from "lucide-react"
+import { Archive, ArchiveX, Clock, MoreVertical, Trash, Trash2 } from "lucide-react"
 import { Button } from "~/components/ui/button"
 import { Separator } from "~/components/ui/separator"
 import useThreads from "~/hooks/use-threads"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "~/components/ui/dropdown-menu"
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar"
+import { format } from "date-fns"
+  
 
 const ThreadDisplay = () => {
     const  {threadId, threads} = useThreads() //from hook
@@ -12,6 +23,7 @@ const ThreadDisplay = () => {
 
   return (
     <div className="flex flex-col h-full">
+        {/* Buttons row */}
         <div className="flex items-center p-2">
             <div className="flex items-center gap-2">
                 <Button variant={'ghost'} size='icon' disabled={!thread}>
@@ -28,11 +40,82 @@ const ThreadDisplay = () => {
             <Button className='ml-2' variant={'ghost'} size='icon' disabled={!thread}>
                     <Clock className="size-4" />
                 </Button>
+            <div className="flex items-center ml-auto">
+            <DropdownMenu>
+                <DropdownMenuTrigger>
+                <Button className='ml-2' variant={'ghost'} size='icon' disabled={!thread}>
+                        <MoreVertical className="size-4" />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Mark as unread</DropdownMenuItem>
+                    <DropdownMenuItem>Star thread</DropdownMenuItem>
+                    <DropdownMenuItem>Add label</DropdownMenuItem>
+                    <DropdownMenuItem>Mute thread</DropdownMenuItem>
+                </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
         </div>
+        <Separator />
+        {thread ? <>
+            <div className="flex flex-col flex-1 overflow-scroll">
+                <div className="flex items-center p-4">
+                    <div className="flex items-center gap-4 text-sm">
+                        <Avatar>
+                            <AvatarImage alt='avatar' />
+                            <AvatarFallback>
+                                {thread.emails[0]?.from?.name?.split(' ').map(chunk => chunk[0]).join('')}
+                            </AvatarFallback>
+                        </Avatar>
+                        <div className="grid gap-1 ">
+                            <div className="font-semibold">
+                                {thread.emails[0]?.from.name}
+                                <div className="text-xs line-clamp-1">
+                                    {thread.emails[0]?.subject}
+                                </div>
+                                <div className="text-xs line-clamp-1">
+                                    <span className="font-medium">
+                                        Reply-To:
+                                    </span>
+                                    {thread.emails[0]?.from.address}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {thread.emails[0]?.sentAt && (
+                        <div className="ml-auto text-xs text-muted-foreground">
+                            {
+                                format(new Date(thread.emails[0]?.sentAt), 'PPpp')
+                            }
+                        </div>
+                    )}
+                </div>
+                <Separator />
+                <div className="max-h-[calc(100vh-500px)] overflow-scroll flex flex-col">
+                    <div className="p-6 flex flex-col gap-4">
+                        {thread.emails.map(
+                            email => {
+                                return <div key={email.id}>
+                                    {email.subject}
+                                </div>
+                            }
+                        )}
+                    </div>
+                </div>
+                <div className="flex-1 ">
 
-        <div className="flex items-center ml-auto">
-
-        </div>
+                </div>
+                <Separator className="mt-auto" />
+                {/* Reply Box */}
+                Reply Box
+            </div>
+        </> : <>
+            <div className="p-8 text-center text-muted-foreground">
+                No message selected
+            </div>
+        </>}
     </div>
   )
 }
