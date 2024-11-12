9cd90b40c1c0c25f20b6ad36980b89bc5519ee15
Mike Cecconello <mikececco2000@gmail.com>
2024-11-12 06:24:07 +0100

    useAccountSwitching for kbar

--- src/components/kbar/index.tsx
+++ src/components/kbar/index.tsx
@@ -6,6 +6,7 @@ import RenderResults from "./render-results";
 import { useAtom } from "jotai";
 import { useLocalStorage } from "usehooks-ts";
 import useThemeSwitching from "./use-theme-switching";
+import useAccountSwitching from "./use-account-switching";
 
 export default function KBar({children}: {children: React.ReactNode}) {
     const [_, setTab] = useLocalStorage(`email-tab`, 'inbox')
@@ -79,6 +80,7 @@ export default function KBar({children}: {children: React.ReactNode}) {
 
 const ActualComponent = ({children}: {children: React.ReactNode}) => {
     useThemeSwitching()
+    useAccountSwitching()
     return <>
         <KBarPortal>
             <KBarPositioner className="fixed inset-0 bg-black/40 dark:bg-black/60 backdrop-blur-sm scrollbar-hide !p-0 z-[999]">
