49b77da806f85491d32e7078dc84006c3a6878c2
Mike Cecconello <mikececco2000@gmail.com>
2024-12-08 16:57:51 +0100

    5:15:45 / 8:28:05

--- src/app/mail/reply-box.tsx
+++ src/app/mail/reply-box.tsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import EmailEditor from './email-editor'
+import useThreads from '~/hooks/use-threads'
+import { api } from '~/trpc/react'
 
 const ReplyBox = () => {
+
+  const [threadId] = useThreads()
+  const { accountId } = useThreads()
+  const { data: replyDetails } = api.account.getReplyDetails.useQuery({
+    accountId: accountId,
+    threadId: threadId || '',
+    // replyType: 'reply'
+  })
+
+}
+
+const Component = () => {
   return (
-    <EmailEditor />
+    <EmailEditor
+            toValues={toValues}
+            ccValues={ccValues}
+
+            onToChange={(values) => { 
+                setToValues(values)
+            }}
+            onCcChange={(values) => {
+                setCcValues(values)
+            }}
+
+            subject={subject}
+            setSubject={setSubject}
+            to={toValues.map(to => to.value)}
+            handleSend={handleSend}
+            isSending={sendEmail.isPending}
+        />
   )
 }
 
